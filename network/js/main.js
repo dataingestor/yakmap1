a9e0177edaf2d30e854465e092849c5ef6ddb13d
tilnow <44685516+tilnow@users.noreply.github.com>
2020-10-30 14:23:48 +0200

    Update main.js

--- network/js/main.js
+++ network/js/main.js
@@ -564,7 +564,7 @@ function nodeActive(a) {
             var d = f.attributes[attr],
                 h = "";
 			if (attr!=image_attribute) {
-                h = '<span><strong>' + attr + ':</strong> ' + d + '_^^_+'</span><br/>'
+                h = '<span><strong>' + attr + ':</strong> ' + d + '_^^_'+'</span><br/>'
 			}
             //temp_array.push(f.attributes[g].attr);
             e.push(h)
